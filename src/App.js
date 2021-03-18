65cecb0013e3dbfe5b8574be8560ccb46840360b
Krish Kapadia <kpkapadia@enacton.com>
2021-03-18 17:55:35 +0530

    React Router  Dom done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Link, Switch, NavLink } from "react-router-dom";
+import TmpFile from './tmpFile';
 
 function App() {
+  const [state, setState] = useState("Krishna");
+
+  const onClickHandler = () => {
+    //alert("Hello");
+    setState(state + 1);
+  }
+
+  const handleInputChange = (event) => {
+    //alert("Hello");
+    //setState(state + 1);
+    console.log("event:", event);
+    setState(event.target.value);
+    var v1 = ["krishna", "kapadia"];
+    var v2 = [...v1, "hello"];
+    console.log("val:", v2);
+  }
+
+  const demo = () => {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Link to="/tmpFile/1" >ClickMe</Link>
+          <NavLink activeClassName="" to="/tmpFile/1" >ClickMe With NavLink</NavLink>
+          <span>{state}</span>
+          <input onChange={handleInputChange} />
+          <button onClick={onClickHandler}>ClickMe</button>
+        </header>
+      </div>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path="/" component={demo} />
+      <Route path="/tmpFile/:id" component={TmpFile} />
+
+    </Switch>
+
   );
 }
 
